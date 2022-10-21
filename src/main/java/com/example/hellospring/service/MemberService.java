a8dea086c024971eb745cd91a05400a25b2a959f
aisha <aisha@gccompany.co.kr>
2022-10-21 15:21:30 +0900

    [회원관리예제-백엔드]회원서비스 개발

---
+++ src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member.getName());
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(String name) {
+        memberRepository.findByName(name)
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 같은 이름의 회원이 존재합니다.");
+            });
+    }
+
+    /**
+     * 전체 멤버 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 1명의 멤버 조회
+     */
+    public Optional<Member> findOne(Long id) {
+        return memberRepository.findById(id);
+    }
+}

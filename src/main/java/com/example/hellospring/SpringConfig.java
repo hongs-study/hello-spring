553347308e4f286f959b78aeb30d7f4b89ae40d0
hong <juheeya401@gmail.com>
2022-10-24 09:49:01 +0900

    [DB접근기술]JPA로 데이터 조회하기

--- src/main/java/com/example/hellospring/SpringConfig.java
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,28 +1,32 @@
 package com.example.hellospring;
 
-import com.example.hellospring.repository.JdbcMemberRepository;
-import com.example.hellospring.repository.JdbcTemplateMemberRepository;
+import com.example.hellospring.repository.JpaMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
 import com.example.hellospring.service.MemberService;
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class SpringConfig {
 
     private final DataSource dataSource;
+    private final EntityManager em;
 
-    public SpringConfig(DataSource dataSource) {
+    @Autowired
+    public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
     @Bean
--- src/main/java/com/example/hellospring/domain/Member.java
+++ src/main/java/com/example/hellospring/domain/Member.java
@@ -1,9 +1,19 @@
 package com.example.hellospring.domain;
 
-public class Member {
-    private Long id;
-    private String name;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
     public Long getId() {
         return id;
     }

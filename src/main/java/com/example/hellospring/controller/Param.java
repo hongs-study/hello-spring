eab2e93f745bb9ed0dd2e6639cbef1f2c2e32d47
aisha <aisha@gccompany.co.kr>
2022-12-28 14:48:24 +0900

    jackson + 날짜 테스트

--- src/main/java/com/example/hellospring/controller/Param.java
+++ src/main/java/com/example/hellospring/controller/Param.java
@@ -1,7 +1,11 @@
 package com.example.hellospring.controller;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Param {
-
-
-
+    private String userName;
+    private Integer userAge;
 }

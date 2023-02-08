43554a7c001106d7db1960eb75d21839220d3586
aisha <aisha@gccompany.co.kr>
2023-02-08 15:16:53 +0900

    [List영속성테스트]Entity에 casecade 설정이 없을 때

--- src/main/java/com/example/hellospring/domain/Team.java
+++ src/main/java/com/example/hellospring/domain/Team.java
@@ -1,34 +1,32 @@
 package com.example.hellospring.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
-import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@ToString
+@ToString(exclude = {"memberList"})
 @Setter
 @Getter
 @Entity
-@Table(name = "temp_member")
-public class Member {
+@Table(name = "temp_team")
+public class Team {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "team_id", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
-    private Team team;
-
     @Column
     private String name;
+
+    @OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
+    private List<Member> memberList = new ArrayList<>();
 }
---
+++ src/test/java/com/example/hellospring/repository/TeamAndMemberTest.java
@@ -0,0 +1,125 @@
+package com.example.hellospring.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.domain.Team;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@DisplayName("1:다 관계 테이블이 이미 있고, '다'테이블에 이미 데이터가 있고, '1'테이블에는 데이터가 없을 때 '1'테이블에 데이터를 나중에 넣었을 때")
+@Transactional
+@SpringBootTest
+public class TeamAndMemberTest {
+
+    @Autowired
+    private SpringDataJpaMemberRepository memberRepository;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    DataSource dataSource;
+    @Autowired
+    private EntityManager em;
+
+    @BeforeEach
+    void beforeEach() {
+        em.createNativeQuery("insert into temp_team (name) values (NULL)").executeUpdate();
+        em.flush();
+        Long lastTeamId = teamRepository.findFirstByOrderByIdDesc().get().getId() + 1;
+        memberRepository.deleteAll();
+        teamRepository.deleteAll();
+
+        String member1 = """
+            insert into temp_member(name, team_id) value('빈스', %d)
+            """.formatted(lastTeamId);
+        String member2 = """
+            insert into temp_member(name, team_id) value('엘빈', %d)
+            """.formatted(lastTeamId);
+        String member3 = """
+            insert into temp_member(name, team_id) value('아이샤', %d)
+            """.formatted(lastTeamId);
+        em.createNativeQuery(member1).executeUpdate();
+        em.createNativeQuery(member2).executeUpdate();
+        em.createNativeQuery(member3).executeUpdate();
+        em.flush();
+        em.clear();
+    }
+
+    @DisplayName("조회 테스트")
+    @Test
+    void testFindMember() {
+        //given
+        List<Member> all = memberRepository.findAll();
+
+        //then
+        assertThat(all).isNotEmpty();
+        all.forEach(System.out::println);
+    }
+
+    @DisplayName("1. casecade 없음")
+    @Test
+    void testJpaTest() {
+        //given
+        Team newTeam = new Team();
+        newTeam.setName("홍길동");
+        Team savedTeam = teamRepository.save(newTeam);
+        Long teamId = savedTeam.getId();
+        em.flush();
+        em.clear();
+
+        //when
+        Team foundTeam = teamRepository.findById(teamId).get();
+
+        //then
+        System.out.println("==================================================");
+        System.out.println("newTeam = " + newTeam);
+        System.out.println("savedTeam = " + savedTeam);
+        System.out.println("foundTeam = " + foundTeam);
+
+        System.out.println("newTeam.getMemberList() = " + newTeam.getMemberList());     // 조회X. newTeam.getMemberList() = []
+        System.out.println("savedTeam.getMemberList() = " + savedTeam.getMemberList()); // 조회X. savedTeam.getMemberList() = []
+        System.out.println("foundTeam.getMemberList() = "
+            + foundTeam.getMemberList()); // 조회O. foundTeam.getMemberList() = [Member(id=1, team=Team(id=10, name=홍길동), name=홍길동), Member(id=2, team=Team(id=10, name=홍길동), name=성춘향), Member(id=3, team=Team(id=10, name=홍길동), name=아이샤)]
+        assertThat(foundTeam.getMemberList()).hasSize(3);
+        // 결론:팀을 저장하기 전/후에는 member가 영속데이터에 포함되어있지 않다. 반드시 find() 하여 조회쿼리가 실행되어야 한다.
+        System.out.println("==================================================");
+    }
+
+
+    @DisplayName("2. casecade 사용")
+    @Test
+    void testJpaTest2() {
+        //given
+        Team newTeam = new Team();
+        newTeam.setName("홍길동");
+        Team savedTeam = teamRepository.save(newTeam);
+        Long teamId = savedTeam.getId();
+        em.flush();
+        em.clear();
+
+        //when
+        Team foundTeam = teamRepository.findById(teamId).get();
+
+        //then
+        System.out.println("==================================================");
+        System.out.println("newTeam = " + newTeam);
+        System.out.println("savedTeam = " + savedTeam);
+        System.out.println("foundTeam = " + foundTeam);
+
+        System.out.println("newTeam.getMemberList() = " + newTeam.getMemberList());     // 조회X. newTeam.getMemberList() = []
+        System.out.println("savedTeam.getMemberList() = " + savedTeam.getMemberList()); // 조회X. savedTeam.getMemberList() = []
+        System.out.println("foundTeam.getMemberList() = "
+            + foundTeam.getMemberList()); // 조회O. foundTeam.getMemberList() = [Member(id=1, team=Team(id=10, name=홍길동), name=홍길동), Member(id=2, team=Team(id=10, name=홍길동), name=성춘향), Member(id=3, team=Team(id=10, name=홍길동), name=아이샤)]
+        // 결론:팀을 저장하기 전/후에는 member가 영속데이터에 포함되어있지 않다. 반드시 find() 하여 조회쿼리가 실행되어야 한다.
+        System.out.println("==================================================");
+    }
+}

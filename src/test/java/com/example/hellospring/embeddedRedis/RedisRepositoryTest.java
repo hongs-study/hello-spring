9a14cd3469913b43912b261a8721eacbab0f52c3
aisha <aisha@gccompany.co.kr>
2023-03-06 17:46:34 +0900

    EmbeddedRedis 로 테스트 - 통테(@SpringBootTest사용하여 모든 빈등록) 대신 필요한 빈만 등록하여 테스트하도록

--- src/test/java/com/example/hellospring/embeddedRedis/RedisRepositoryTest.java
+++ src/test/java/com/example/hellospring/embeddedRedis/RedisRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.hellospring.embeddedRedis;
 
+import com.example.hellospring.RedisConfig;
 import com.example.hellospring.repository.RedisRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@DataJpaTest
-@SpringBootTest(classes = TestRedisConfiguration.class)
+// 필요한 빈만 등록하여 테스트 : classes에 지정한 클래스만 빈등록하여 테스트를 실행한다. 즉, 전체 빈등록보다 빠르다
+@SpringBootTest(classes = {TestRedisConfiguration.class, RedisRepository.class, RedisConfig.class})
 class RedisRepositoryTest {
 
     @Autowired
@@ -18,10 +19,12 @@ class RedisRepositoryTest {
     public void saveAndFind() {
         String value = "hello world!";
         String key = redisRepository.save(value);
+        System.out.println("key = " + key + ", value = " + value);
 
-        String byKey = redisRepository.findByKey(key);
+        String findValue = redisRepository.findByKey(key);
+        System.out.println("findValue = " + findValue);
 
-        assertThat(byKey).isEqualTo(value);
+        assertThat(findValue).isEqualTo(value);
     }
 
 }
